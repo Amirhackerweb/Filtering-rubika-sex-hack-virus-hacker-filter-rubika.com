aad054f68737d42f2ba54c7c14e9698cd103b63c
Amirhackerweb <136385425+Amirhackerweb@users.noreply.github.com>
2023-06-14 01:09:54 +0330

    Create Filter-rubika.py

---
+++ Filter-rubika.py
@@ -0,0 +1,7 @@
+Import "sex-filter" 
+Import "hack-rubika"
+print="hacker-sex-infect-virus-net-report-police-cyber-filter-rubika.py"
+#sex.gif.php.hack.net.report.filter.rubika.io
+<img src="https://s8.uupload.ir/files/20230613_130717_xc9x.jpg">
+print=("https://cyberpolice.ir") 
+<img src="https://s8.uupload.ir/files/20230613_130653_xe3r.jpg">
