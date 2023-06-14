206b7e984f3e98b0ee75f6dacbcca6e083b995bd
Amirhackerweb <136385425+Amirhackerweb@users.noreply.github.com>
2023-06-14 12:18:17 +0330

    Create Filter.py

---
+++ Filter.py
@@ -0,0 +1,8 @@
+# Yftt15k-Filtering_rubika.h-dscript
+print ("http://pronhun.com https://s2.uupload.ir/files/3_sib0.png https://s2.uupload.ir/files/4_2yls.png https://s6.uupload.ir/files/images_(2)_mvhd.jpeg https://s6.uupload.ir/files/images_(1)_b2am.jpeg https://xexx.com")
+......... 
+Import sex
+Import rubika
+Import sex
+Import yftt15k
+Import filter-rubika.io
